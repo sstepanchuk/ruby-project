45aa141f601cd9dda5122da7153751a9706d96c6
Serhii Stepanchuk <96484585+sstepanchuk@users.noreply.github.com>
2023-11-28 13:21:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-bundle exec rails server -b '0.0.0.0' -p 3000
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /myapp/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
